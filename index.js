6713c741959721cad981e43cde6d4dae90dc679b
Wen Xiao <wennstyle@gmail.com>
2021-11-10 00:55:18 -0500

    commit tree 182702351be221d78662ec02ed3312d692828941

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-function hasTargetSum(array, target) {
+function hasTargetSum(array, target){
   // Write your algorithm here
-}
+  let newArray = [...array]
+  let finalArray = []
+    
+     for (i =0; i < array.length; i++) {
+       newArray.shift()
+      for(j = 0; j<newArray.length; j++){
+        finalArray.push(array[i]+newArray[j])
+      }
+     }
+  //return finalArray
+  
+  let result = finalArray.includes(target) //goes through array to see if the target value is in the array, assign a true or false value to the variable result
+  return result
+    
+  }
 
 /* 
   Write the Big O time complexity of your function here
@@ -8,6 +22,16 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  Prompt: functions takes in two arugments, one an array of numbers and one a target (also a number). Adds all pairs of numbers and see if it equals to target, if there's one case that is true, return true, if not return false
+  solution 1:
+  1. use target to subtract a number from the array, compare this result with all the numbers in the array. if there's a match then return true.
+  2. if the first iteration is false, then continue through all numbers (perhaps we sort the array?)
+  3. after all iteration and there is not 'true' then return false
+
+  solution 2:
+  1. Add up all number pairs and put it into a new array. Make loop so that numbers will not add each other twice
+  2. if we can find the target number in the new array, then return true, otherwise return false
+
 */
 
 /*
